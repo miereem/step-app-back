e36db547ba53ebe8f0662b1992a913c0a0352980
miereem <mabdurasulkyzy@gmail.com>
2023-12-13 04:31:07 +0300

    search added

--- routes/index.js
+++ routes/index.js
@@ -1,166 +1,193 @@
-const express = require('express');
-const cors = require('cors');
-const {Sequelize} = require("sequelize");
+const express = require("express");
+const cors = require("cors");
+const { Sequelize } = require("sequelize");
+const { Op } = require("sequelize");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Place = require("../models/Place")
-const cookieParser = require('cookie-parser');
-const multer = require('multer');
-const fs = require('fs');
+const Place = require("../models/Place");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const fs = require("fs");
 
+const sequelize = new Sequelize(
+  "postgres://s343056:a8x3u3Xyid768O1I@localhost:8933/studs"
+);
 
-const sequelize = new Sequelize('postgres://s343056:a8x3u3Xyid768O1I@localhost:8933/studs');
-
-const jwtSecret = ('sdlkkjdfgksjlkl');
+const jwtSecret = "sdlkkjdfgksjlkl";
 const bcryptSalt = bcrypt.genSaltSync(10);
-
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static('/Users/home/step-app/uploads'));
+app.use("/uploads", express.static("/Users/home/step-app/uploads"));
 
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:5173'
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-      await sequelize.authenticate();
-      res.json('Connection to db has been established successfully.');
+    await sequelize.authenticate();
+    res.json("Connection to db has been established successfully.");
   } catch (err) {
-      console.error('Unable to connect to the database:', err);
-      res.status(500).send('Internal Server Error');
+    console.error("Unable to connect to the database:", err);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-
-app.post('/login', async (req, res) => {
-    const {username, password} = req.body;
-    const user = await User.findOne({where: {username: username}});
-    if(user) {
-        if(bcrypt.compare(password, user.password)) {
-            jwt.sign({username: user.username, id: user.id}, jwtSecret, {},
-                (err, token) => {
-                    if (err) throw err;
-                    res.cookie('token', token).json(user);
-                });
-        } else {
-            res.status(422).json('pass wrong');
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ where: { username: username } });
+  if (user) {
+    if (bcrypt.compare(password, user.password)) {
+      jwt.sign(
+        { username: user.username, id: user.id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(user);
         }
-
+      );
     } else {
-        res.json('not found');
+      res.status(422).json("pass wrong");
     }
+  } else {
+    res.json("not found");
+  }
 });
 
-
-app.post('/register', async (req, res) => {
-    const {name, email, username, password} = req.body;
-    await User.sync();
-    try {
-        const user = await User.create({
-            name,
-            email,
-            username,
-            password:bcrypt.hashSync(password, bcryptSalt)
-        });
-        res.json({user});
-    } catch (e) {
-        res.status(422).json(e);
-    }
-});
-
-
-app.get('/profile',  (req, res) =>
-{
-    const{token} = req.cookies;
-    if(token) {
-        jwt.verify(token, jwtSecret, {}, async (err, user) => {
-            if(err) throw err;
-            const {name, email, username, id} = await User.findByPk(user.username);
-            res.json({name, email, username, id});
-        });
-    } else {
-        res.json(null);
-    }
-
-});
-
-app.post('/logout', (req, res) => {
-    res.cookie('token', '').json(true);
-})
-
-const PhotosMiddleWare = multer({dest:'../uploads/'});
-app.post('/upload', PhotosMiddleWare.array('photos', 100), (req, res) => {
-    const uploadedFiles = [];
-    for(let i = 0; i < req.files.length; i++) {
-        const {path, originalname} = req.files[i];
-        const parts = originalname.split('.');
-        const newPath = path + '.' + parts[parts.length-1];
-        fs.renameSync(path, newPath);
-        uploadedFiles.push(newPath.replace('../uploads/',''));
-    }
-    console.log(uploadedFiles);
-    res.json(uploadedFiles);
-})
-
-app.post('/places', (req, res) => {
-    const {token} = req.cookies;
-    const {title, address, addedPhotos, opensAt, closesAt, description} = req.body;
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-        if (err) throw err;
-        await Place.sync();
-        const placeDoc = await Place.create({
-            owner:userData.username,
-            title: title, address, photos:addedPhotos,
-            openingTime:opensAt, closingTime:closesAt, description
-        });
-        res.json(placeDoc);
+app.post("/register", async (req, res) => {
+  const { name, email, username, password } = req.body;
+  await User.sync();
+  try {
+    const user = await User.create({
+      name,
+      email,
+      username,
+      password: bcrypt.hashSync(password, bcryptSalt),
     });
+    res.json({ user });
+  } catch (e) {
+    res.status(422).json(e);
+  }
 });
 
-app.get('/user-places', (req, res) => {
-    //sequelize.authenticate();
-    const {token} = req.cookies;
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-        const {username} = userData;
-        const resp = await Place.findAll({where: {owner:username}});
-        res.json(resp);
-
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, user) => {
+      if (err) throw err;
+      const { name, email, username, id } = await User.findByPk(user.username);
+      res.json({ name, email, username, id });
     });
-})
-
-
-
-app.get('/places/:id', async (req,res) => {
-    const {id} = req.params;
-    res.json(await Place.findByPk(id));
+  } else {
+    res.json(null);
+  }
 });
 
-app.put('/places', async (req, res) =>{
-    const {token} = req.cookies;
-    const {id, title, address, addedPhotos, opensAt, closesAt, description} = req.body;
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-        const placeDoc = await Place.findByPk(id);
-        if (userData.username === placeDoc.owner) {
-            placeDoc.set({
-                title:title, address:address, photos: addedPhotos, openingTime:opensAt, closingTime:closesAt, description:description
-            });
-            await placeDoc.save();
-            res.json('ok');
-        }
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+
+const PhotosMiddleWare = multer({ dest: "../uploads/" });
+app.post("/upload", PhotosMiddleWare.array("photos", 100), (req, res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
+    const newPath = path + "." + parts[parts.length - 1];
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace("../uploads/", ""));
+  }
+  console.log(uploadedFiles);
+  res.json(uploadedFiles);
+});
+
+app.post("/places", (req, res) => {
+  const { token } = req.cookies;
+  const { title, address, addedPhotos, opensAt, closesAt, description } =
+    req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    await Place.sync();
+    const placeDoc = await Place.create({
+      owner: userData.username,
+      title: title,
+      address,
+      photos: addedPhotos,
+      openingTime: opensAt,
+      closingTime: closesAt,
+      description,
     });
+    res.json(placeDoc);
+  });
 });
 
-
-app.get('/places', async (req, res) => {
-   res.json( await Place.findAll());
+app.get("/user-places", (req, res) => {
+  //sequelize.authenticate();
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { username } = userData;
+    const resp = await Place.findAll({ where: { owner: username } });
+    res.json(resp);
+  });
 });
 
-app.listen(3000, () => { console.log('Server running on port 3000');});
+app.get("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  res.json(await Place.findByPk(id));
+});
 
+app.put("/places", async (req, res) => {
+  const { token } = req.cookies;
+  const { id, title, address, addedPhotos, opensAt, closesAt, description } =
+    req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const placeDoc = await Place.findByPk(id);
+    if (userData.username === placeDoc.owner) {
+      placeDoc.set({
+        title: title,
+        address: address,
+        photos: addedPhotos,
+        openingTime: opensAt,
+        closingTime: closesAt,
+        description: description,
+      });
+      await placeDoc.save();
+      res.json("ok");
+    }
+  });
+});
 
+app.get("/places", async (req, res) => {
+  res.json(await Place.findAll());
+});
 
+app.get("/search", async (req, res) => {
+  try {
+    const { query } = req.query;
+    const places = await Place.findAll({
+      where: {
+         [Op.or]: {
+            title: { [Op.iLike]:  '%' + query + '%' } ,
+            address: { [Op.iLike]: '%' + query + '%' } ,
+            
+         }
+       
+      },
+    });
+    res.json(places);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
